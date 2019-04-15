1558e2377378e9cb932712facb1eb89cddf552bb
sawkun <saru931aaa@yahoo.co.jp>
2019-04-15 21:24:57 +0000

    ログイン用のcreateメソッド　log_in user →　redirect_to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
       render 'new'
